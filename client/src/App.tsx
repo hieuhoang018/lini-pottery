a0e288b528ddc5560b754e521e18230d15552daf
Hoang Vu Hieu <hieu.hoang@tuni.fi>
2026-04-28 13:13:01 +0300

    feat: implemented cart page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import { ShopPage } from "./pages/ShopPage"
 import { ProductDetailPage } from "./pages/ProductDetailPage"
+import { CartPage } from "./pages/CartPage"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ShopPage />} />
       <Route path="/products/:slug" element={<ProductDetailPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Routes>
   )
 }
